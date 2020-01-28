2f15086c6ac513162ac396a36c2d46493f0182e5
Logan06250 <logan.lamouar@viacesi.fr>
2020-01-28 16:49:27 +0100

    Init

---
+++ src/components/Gallery.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="Gallery">
+        <img class="main_img" src="../assets/mainimg.jpg"/>
+    </div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+.main_img {
+    width: 100%;
+    height: 100%;
+}
+
+</style>
